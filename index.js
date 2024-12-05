f5063c27633152e3dc31f36a757b3b8d90b92ac4
unknown <m7amed322>
2024-12-05 13:18:26 +0200

    v2

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-require("./startup/logging")();
-require("./startup/config")();
-require("./startup/routes")(app);
-require("./startup/db")();
-app.listen(process.env.PORT||1000, () => {
-  console.log("listening to port 1000 ...");
+// require("./startup/logging")();
+// require("./startup/config")();
+// require("./startup/routes")(app);
+// require("./startup/db")();
+port = process.env.PORT||1000;
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+app.listen(port, () => {
+  console.log(`listening to port ${port} ...`);
 });
