d5db457bd512ae2321c5ebf657ea6d2cbdf526cd
unknown <m7amed322>
2024-12-05 13:28:20 +0200

    v3

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
-// require("./startup/logging")();
-// require("./startup/config")();
-// require("./startup/routes")(app);
-// require("./startup/db")();
-port = process.env.PORT||1000;
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+
+require("./startup/logging")();
+require("./startup/config")();
+require("./startup/routes")(app);
+require("./startup/db")();
+
+const port = process.env.PORT || 1000; // Use Netlify's port or default to 1000
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
 });
+
 app.listen(port, () => {
-  console.log(`listening to port ${port} ...`);
+    console.log(`listening to port ${port}`);
 });
