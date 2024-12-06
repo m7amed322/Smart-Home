ef0cd3dc6ff59466ab9fc578e5f8b0b224eab454
unknown <m7amed322>
2024-12-07 00:42:15 +0200

    d5

--- startup/db.js
+++ startup/db.js
@@ -1,19 +1,10 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
-
-let isConnected = false;
-
 module.exports = async function () {
-    if (isConnected) {
-        winston.info("MongoDB connection reused.");
-        return;
-    }
-
     try {
-        const connection = await mongoose.connect(
+        await mongoose.connect(
             `mongodb+srv://mohamedabdalslam678:${process.env.db_password}@graduationproject.hspls.mongodb.net/?retryWrites=true&w=majority&appName=graduationProject`
         );
-        isConnected = !!connection.connections[0].readyState;
         winston.info("MongoDB connected successfully.");
     } catch (err) {
         winston.error("MongoDB connection failed.", err);
