dfc3e360e474a6ab2f53a18133a9a2fdd0600bab
unknown <m7amed322>
2024-12-06 22:27:56 +0200

    v5

--- passport.js
+++ passport.js
@@ -1,5 +1,5 @@
 const passport = require("passport");
-const { User } = require("./models/user");
+const { User } = require("/models/user");
 const googlePlusTokenStrategy = require("passport-google-plus-token");
 passport.use(
   "googleToken",
