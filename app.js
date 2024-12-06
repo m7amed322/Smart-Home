b58413155f32949f89b0f7db713cf68a667aa852
unknown <m7amed322>
2024-12-07 01:23:22 +0200

    d6

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ require("./startup/logging")();
 require("./startup/config")();
 require("./startup/routes")(app);
 require("./startup/db")();
-
+// app.listen(1000,()=>{
+//     console.log("1000 ...")
+// })
 module.exports.handler = serverless(app);
--- startup/db.js
+++ startup/db.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
-module.exports = async function () {
-    try {
-        await mongoose.connect(
+module.exports = function () {
+    
+        mongoose.connect(
             `mongodb+srv://mohamedabdalslam678:${process.env.db_password}@graduationproject.hspls.mongodb.net/?retryWrites=true&w=majority&appName=graduationProject`
-        );
-        winston.info("MongoDB connected successfully.");
-    } catch (err) {
-        winston.error("MongoDB connection failed.", err);
-        throw err;
-    }
+        ).then(()=>winston.info("MongoDB connected successfully.")).catch((err)=>{winston.error(err)})
+        
+    
 };
