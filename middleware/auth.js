b2146e1d3d616230858bd176aaa35e4da29408aa
unknown <m7amed322>
2024-12-08 19:57:33 +0200

    v

--- middleware/auth.js
+++ middleware/auth.js
@@ -20,13 +20,15 @@ module.exports = async function (req, res, next) {
     let admin = await Admin.findOne({_id:payload.id})
     if(payload.isAdmin){
       const jwt=await bcrypt.compare(token,admin.jwt);
-      if(!jwt || admin.jwtExpires<Date.now()){
+      admin = await Admin.findOne({jwt:jwt , jwtExpires:{$gt:Date.now()}})
+      if(!jwt|| !admin){
         res.status(401).json({error:"not valid token or token is expired"});
       }
     }
     else{
     const jwt=await bcrypt.compare(token,user.jwt);
-    if(!jwt||user.jwtExpires<Date.now()){
+    user = await User.findOne({jwt:jwt , jwtExpires:{$gt:Date.now()}})
+    if(!jwt||!user){
       res.status(401).json({error:"not valid token or token is expired"});
     }
   }
