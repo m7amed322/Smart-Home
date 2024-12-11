ffdeaf60c1e0df6cff2d729e881a2c13bca5159f
unknown <m7amed322>
2024-12-11 21:23:17 +0200

    v

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,37 +1,39 @@
 const jwt = require("jsonwebtoken");
-const {User}=require("../models/user");
-const {Admin}=require("../models/admin");
+const { User } = require("../models/user");
+const { Admin } = require("../models/admin");
 // require("dotenv").config();
-const crypto = require("crypto")
+const crypto = require("crypto");
+const { error } = require("console");
 module.exports = async function (req, res, next) {
   const token = req.header("x-auth-token");
-  if( !token )
-  {
-    res.status(401).json({error:"accses denied, no token provided "});
+  if (!token) {
+    res.status(401).json({ error: "accses denied, no token provided " });
   }
   try {
     const payload = jwt.verify(token, process.env.jwtPrivateKey);
   } catch (err) {
-    res.status(400).json({error:"not valid token"});
+    res.status(400).json({ error: "not valid token" });
+    next(err);
   }
-    let payload = jwt.verify(token, process.env.jwtPrivateKey);
-    
-    if(payload.isAdmin){
-      let admin = await Admin.findOne({_id:payload.id})
-      hashedToken = crypto.createHash('sha256').update(token).digest('hex');
-      if(admin.jwt!=hashedToken || admin.jwtExpires<Date.now()){
-        res.status(401).json({error:"not valid token or token is expired"});
-      }
-      req.tokenPayload = payload;
-      next();
-    }
-    else{
-    let user = await User.findOne({_id:payload.id})
-    hashedToken=crypto.createHash('sha256').update(token).digest('hex');
-    if(user.jwt!=hashedToken||user.jwtExpires<Date.now()){
+  let payload = jwt.verify(token, process.env.jwtPrivateKey);
+
+  if (payload.isAdmin) {
+    let admin = await Admin.findOne({ _id: payload.id });
+    hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+    if (admin.jwt != hashedToken || admin.jwtExpires < Date.now()) {
       res.status(401).json({error:"not valid token or token is expired"});
+      return;
     }
     req.tokenPayload = payload;
-      next();
+    next();
+  } else {
+    let user = await User.findOne({ _id: payload.id });
+    hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+    if (user.jwt != hashedToken || user.jwtExpires < Date.now()) {
+      res.status(401).json({error:"not valid token or token is expired"});
+      return;
+    }
+    req.tokenPayload = payload;
+    next();
   }
 };
