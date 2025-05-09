caed6fdafbebbeaf6f7cec35d16fc13cf40502a0
unknown <m7amed322>
2025-05-09 20:22:19 +0300

    V

--- models/user.js
+++ models/user.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
   passwordResetToken: String,
   passwordResetTokenExpires: Date,
   passwordChangeAt: Date,
-  isActive: { type: Boolean, default: true },
+  isActive: { type: Boolean, default: false },
   jwt: String,
   jwtExpires: Date,
   userProfilePic: String,
