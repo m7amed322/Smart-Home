66c36c6aa23e82e2ecd10f5609b6a6c0ed6945d5
unknown <m7amed322>
2025-05-09 21:41:56 +0300

    V

--- middleware/error.js
+++ middleware/error.js
@@ -4,15 +4,15 @@ module.exports = function (err, req, res, next) {
     stack: err.stack,
   });
   if (err.message == "access denied") {
-    res.status(403).json({ erorr_message: err.message });
+    res.status(403).json({ error_message: err.message });
     return;
   }
   if (err.message == "unauthorized") {
-    res.status(401).json({ erorr_message: err.message });
+    res.status(401).json({ error_message: err.message });
     return;
   }
   if (new RegExp(".*\\bfound\\b.*", "i").test(err.message)) {
-    res.status(404).json({ erorr_message: err.message });
+    res.status(404).json({ error_message: err.message });
   }
-  res.status(400).json({ erorr_message: err.message });
+  res.status(400).json({ error_message: err.message });
 };
